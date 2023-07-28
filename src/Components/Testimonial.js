73abfb58731b60e567798f18a36dd132eafb7670
Raafat-Saleh <rafat.saleh1313@gmail.com>
2023-07-29 00:06:39 +0300

    testimionial & work component

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,39 @@
+/** @format */
+
+import React from "react";
+import ProfilePic from "../Assets/avater.png";
+
+import { AiFillStar } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
+
+const Testimonial = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiOutlineStar />
+        </div>
+        <h2>Aaaa Bbb</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
